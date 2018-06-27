a7f37a35f0f236c164ff1996c280b6d3ac2e204c
Samson Akol <akolson2010@gmail.com>
2018-06-27 09:04:49 +0300

    Initial project setup commit

---
+++ app/src/main/java/com/journal/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.journal.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
